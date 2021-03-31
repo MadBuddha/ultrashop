31599294efd8cf884775d06410c9aeff959a836c
jmrve <jmrventura92@gmail.com>
2021-03-31 19:50:39 +0100

    Implementado funcionalidade para remover artigos do carrinho

--- cart.php
+++ cart.php
@@ -49,6 +49,31 @@
 			border-collapse: collapse;
 		}
 	</style>
+    <script>
+    document.addEventListener("DOMContentLoaded", () =>{
+        const removeButtons = document.querySelectorAll(".remove");
+
+        for(let button of removeButtons){
+            button.addEventListener("click", ()=>{
+                const product_id = button.dataset.product_id;
+
+                fetch("requests.php", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type":"application/x-www-form-urlencoded"
+                    },
+                    body: "request=removeProduct&product_id=" + product_id
+                })
+                .then(response => response.json() )
+                .then(parsedResponse => {
+                    if (parsedResponse.status =="OK"){
+                        button.parentNode.parentNode.remove();
+                    }
+                });
+            })
+        }
+    })
+    </script>
 </head>
 <body>
 <?php 
@@ -60,6 +85,7 @@
                 <th>Quantidade</th>
                 <th>Preço</th>
                 <th>Total</th>
+                <th>Apagar</th>
             </tr>
 
             <?php
@@ -75,6 +101,9 @@
                     <td>' .$item["quantity"]. '</td>
                     <td>'.$item["price"].'€</td>
                     <td><span class="subtotal">'.$subtotal.'</span>€</td>
+                    <td>
+                    <button data-product_id="' .$item["product_id"]. '" type="button" class="remove">X</button>
+                    </td>
                 </tr>
 
                 ';
